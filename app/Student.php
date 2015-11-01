f1f9beb21e66b30d8337ef96f7007c63f48e80ec
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2015-11-01 22:55:14 +0800

    Added save function

--- app/Student.php
+++ app/Student.php
@@ -3,8 +3,31 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model as Eloquent;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Student extends Eloquent
 {
+    use SoftDeletes;
     //
+    protected $table = 'students';
+    protected $dates = ['deleted_at'];
+    protected $fillable = ['firstname', 'middlename', 'lastname',
+        'contact', 'address', 'dob', 'gender'];
+
+
+    public static function storeStudent($request)
+    {
+        $student = new Student();
+        $student->firstname = $request->get('fname');
+        $student->middlename = $request->get('mname');
+        $student->lastname = $request->get('lname');
+        $student->contact = $request->get('contact');
+        $student->address = $request->get('address');
+        $student->dob = $request->get('dob');
+        $student->gender = $request->get('gender');
+
+        if($student->save()) {
+            return redirect('student/exam', compact('student'));
+        }
+    }
 }
