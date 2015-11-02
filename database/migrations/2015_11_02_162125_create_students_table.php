4279f739d19fbf05d73159ae8f83991ffecfbaf6
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2015-11-02 21:41:52 +0800

    Added create_students_table migration

---
+++ database/migrations/2015_11_02_162125_create_students_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('students', function(Blueprint $blueprint) {
+            $blueprint->increments('id');
+            $blueprint->string('student_id');
+            $blueprint->timestamps();
+            $blueprint->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
