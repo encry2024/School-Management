ac666047d3e8e2299db728b631c5eafa7c206097
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2015-11-02 15:42:14 +0800

    Added save examiner function

--- app/Student.php
+++ app/Student.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model as Eloquent;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use Illuminate\Support\Facades\DB;
 class Student extends Eloquent
 {
     use SoftDeletes;
@@ -12,12 +12,37 @@ class Student extends Eloquent
     protected $table = 'students';
     protected $dates = ['deleted_at'];
     protected $fillable = ['firstname', 'middlename', 'lastname',
-        'contact', 'address', 'dob', 'gender'];
+        'contact', 'address', 'dob', 'gender', 'mother_name', 'mother_work_add', 'mother_num', 'mother_work_occ',
+        'father_name', 'father_work_add', 'father_work_occ'];
+    public $timestamps = true;
 
 
     public static function storeStudent($request)
     {
-        $student = new Student();
+        $examiners = DB::connection('mysql2')->table('examiners')
+            ->insert([
+                'fname'           =>    $request->get('fname'),
+                'mname'           =>    $request->get('mname'),
+                'lname'           =>    $request->get('lname'),
+                'contact'         =>    $request->get('contact'),
+                'p_address'       =>    $request->get('p_address'),
+                'dob'             =>    $request->get('dob'),
+                'gender'          =>    $request->get('gender'),
+                'mother_name'     =>    $request->get('mother_name'),
+                'mother_num'      =>    $request->get('mother_num'),
+                'mother_work_add' =>    $request->get('mother_work_add'),
+                'mother_occ'      =>    $request->get('mother_occ'),
+                'father_name'     =>    $request->get('father_name'),
+                'father_num'      =>    $request->get('father_num'),
+                'father_occ'      =>    $request->get('father_occ'),
+                'father_work_add' =>    $request->get('father_work_add'),
+                'created_at'      =>    (new \DateTime())->format('Y-m-d H:i:s'),
+                'updated_at'      =>    (new \DateTime())->format('Y-m-d H:i:s')
+            ]);
+
+        return redirect('student/exam');
+        
+        /*$student = new Student();
         $student->firstname = $request->get('fname');
         $student->middlename = $request->get('mname');
         $student->lastname = $request->get('lname');
@@ -28,6 +53,6 @@ class Student extends Eloquent
 
         if($student->save()) {
             return redirect('student/exam', compact('student'));
-        }
+        }*/
     }
 }
