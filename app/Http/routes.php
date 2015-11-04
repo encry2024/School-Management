23db147785ae26e458a3cba13ac32924789b874a
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2015-11-04 16:17:47 +0800

    Added route for create exam

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,29 +6,25 @@ Route::controllers([
 ]);
 
 get('/', ['as' => '/', 'uses' => 'HomeController@index']);
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
 
 // Student Route
 Route::bind('student',    function ($id) { return App\Student::whereId($id)->first(); });
 Route::resource('student', 'StudentController');
-
-get('examiner/{eid}', ['as' => 'stud_exam', 'uses' => 'StudentController@entranceExamForm']);
-post('submit_exam', ['as' => 'submit_exam', 'uses' => 'StudentController@submitExam']);
+    # Defined routes
+    get('examiner/{eid}', ['as' => 'stud_exam', 'uses' => 'StudentController@entranceExamForm']);
+    post('submit_exam', ['as' => 'submit_exam', 'uses' => 'StudentController@submitExam']);
 
 
 // User Route
 Route::bind('user', function ($id) { return App\User::whereId($id)->first(); });
 Route::resource('user', 'UserController');
 
+
 // Subject Route
 Route::bind('subject', function ($id) {return App\Subject::whereId($id)->first(); });
 Route::resource('subject', 'SubjectController');
+
+
+// Exam Route
+Route::bind('exam', function ($id) { return App\Exam::whereId($id)->first(); });
+Route::resource('exam', 'ExamController');
